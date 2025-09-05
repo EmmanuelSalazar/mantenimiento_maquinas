a506e4611ae14d531621ead5aa47d1e5d442782d
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2025-09-05 10:23:05 -0500

    lil del

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/historial" element={<HistoryPage />} />
             <Route path="/historial/:id" element={<HistoryPage />} />
-            <Route path="/nueva-intervencion" element={<InterventionForm />} />
             <Route path="/nueva-intervencion/:id" element={<InterventionForm />} />
             <Route path="/qr-generator" element={<QrGenerator />} />
           </Routes>
